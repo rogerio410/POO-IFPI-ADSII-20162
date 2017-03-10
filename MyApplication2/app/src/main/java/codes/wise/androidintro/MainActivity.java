3d95b6bdf2037dbff943c9847680d4f88f4c2a20
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2017-03-10 09:24:28 -0300

    AndroiIntro: Lifecycle and custom toast

--- MyApplication2/app/src/main/java/codes/wise/androidintro/MainActivity.java
+++ MyApplication2/app/src/main/java/codes/wise/androidintro/MainActivity.java
@@ -3,11 +3,19 @@ package codes.wise.androidintro;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private final String TAG = "Activity1";
 
     private Button btnSend;
     private EditText edMessage;
@@ -16,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate");
 
         //Binding
         btnSend = (Button) findViewById(R.id.botao_send);
@@ -35,6 +45,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnSend.setOnClickListener(this);
 
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.customtoast,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText("This is a custom toast");
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "OnResume");
     }
 
     public void abrirTela2(View pipoca){
@@ -48,5 +84,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (view.getId() == R.id.botao_send){
 
         }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
     }
 }
--- MyApplication2/app/src/main/java/codes/wise/androidintro/SegundaActivity.java
+++ MyApplication2/app/src/main/java/codes/wise/androidintro/SegundaActivity.java
@@ -3,9 +3,12 @@ package codes.wise.androidintro;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
+
+    private final String TAG = "Activity2";
 
     private TextView tvPacote;
 
@@ -14,6 +17,8 @@ public class SegundaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
 
+        Log.i(TAG, "onCreate");
+
         tvPacote = (TextView) findViewById(R.id.pacote_recebido);
 
         Intent intent = getIntent();
@@ -21,5 +26,41 @@ public class SegundaActivity extends AppCompatActivity {
 
         tvPacote.setText(pacote);
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
     }
 }
